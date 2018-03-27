0409431b896d3b9c0b1f58e1f10a8550ff363c08
Jorge Herrera <jorge.herrera@shazam.com>
2018-03-26 22:01:47 -0700

    added tox/pytest

---
+++ tests/notebook_test.py
@@ -0,0 +1,7 @@
+import pytest
+
+from terminal_velocity.notebook import *
+
+
+def test_fake():
+    assert True
---
+++ tests/urwid_ui_test.py
@@ -0,0 +1,8 @@
+import pytest
+
+from terminal_velocity.urwid_ui import *
+
+
+def test_fake():
+    assert True
+
